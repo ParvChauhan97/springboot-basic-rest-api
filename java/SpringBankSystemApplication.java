7517ae54fc9a975bf6247a1e2a28f5f5e1afb22b
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-05-04 00:30:49 +0530

    Update SpringBankSystemApplication.java

--- java/SpringBankSystemApplication.java
+++ java/SpringBankSystemApplication.java
@@ -1,1 +1,13 @@
+package org.example.springsyatem.springbank;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBankSystemApplication.class, args);
+    }
+
+}
