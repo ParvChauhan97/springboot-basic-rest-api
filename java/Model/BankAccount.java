2ea77e4fdfe3aacea0abeb1ac481eadb7d975411
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-05-09 11:17:05 +0530

    Create BankAccount class with basic fields
    Added BankAccount class with accountNumber, name, and balance fields.

--- java/Model/BankAccount.java
+++ java/Model/BankAccount.java
@@ -1,1 +1,9 @@
+package org.example.springsyatem.springbank.Model;
 
+public class BankAccount {
+    private long accountNumber;
+    private String name;
+    private double balance;
+
+    public BankAccount() {
+    }
