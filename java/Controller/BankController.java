b342862548f18b189d8ca4fd688d77649b7d3b8c
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-05-11 20:52:55 +0530

    Add BankController with basic structure

--- java/Controller/BankController.java
+++ java/Controller/BankController.java
@@ -1,1 +1,12 @@
+package org.example.springsyatem.springbank.Controller;
 
+import org.example.springsyatem.springbank.Service.BankService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
+public class BankController {
+  
+}
