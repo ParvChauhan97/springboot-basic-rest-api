35f0fed36bf771962fef44229497704a6dd92381
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-05-12 21:18:21 +0530

    Add deposit, withdraw, transfer, and display methods

--- java/Service/BankService.java
+++ java/Service/BankService.java
@@ -15,5 +15,48 @@ public class BankService {
         }
         return false;
     }
+
+    public boolean deposit(long accountNumber, double amount) {
+        if (map.containsKey(accountNumber)) {
+            BankAccount acc = map.get(accountNumber);
+                acc.deposit(amount);
+                return true;
+        } return false;
+    }
+
+    public boolean withdraw(long accountNumber, double amount) {
+        if (map.containsKey(accountNumber)) {
+            BankAccount acc = map.get(accountNumber);
+            if(amount <= acc.getBalance()) {
+                acc.withdraw(amount);
+                return true;
+            }
+        } return false;
+    }
+
+    public boolean transfer(long sender, long receiver, double amount) {
+        if(map.containsKey(sender) && map.containsKey(receiver)) {
+                BankAccount s = map.get(sender);
+                BankAccount r = map.get(receiver);
+                if (amount <= s.getBalance()) {
+                    s.withdraw(amount);
+                    r.deposit(amount);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    public String display(long accountNumber) {
+        if(map.containsKey(accountNumber)) {
+            BankAccount account = map.get(accountNumber);
+            return account.display();
+        }
+        return "Enter Valid Account Number";
+    }
+
+    public Map<Long, BankAccount> displayall(){
+        return map;
+    }
     
 }
