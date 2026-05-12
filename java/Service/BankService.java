2c8a9b7661be4af9f7d77da34450fdaec220aee7
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-05-12 21:13:14 +0530

    Add BankService class for managing bank accounts

--- java/Service/BankService.java
+++ java/Service/BankService.java
@@ -1,1 +1,11 @@
+package org.example.springsyatem.springbank.Service;
+import org.example.springsyatem.springbank.Model.BankAccount;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+@Service
+public class BankService {
+    private HashMap<Long, BankAccount> map = new HashMap<>();
+  
+}
