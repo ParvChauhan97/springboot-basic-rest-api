276ae10f808c4f3094eb0fd3920a60eb241fea63
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-29 22:55:35 +0530

    Update in BankController.java

--- java/Controller/BankController.java
+++ java/Controller/BankController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController 
 @RequestMapping("/account")
 public class BankController {
 
